ce461c0c321d2651485dcf671e51bb269097bf0e
vngocvan@gmail.com <vngocvan@gmail.com@4e6c4620-98ee-d95f-68b7-dc843251c447>
2014-11-18 06:00:10 +0000

    Ini commit

---
+++ src/botvn/RemoteConstants.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package botvn;
+
+/**
+ *
+ * @author vanvo
+ */
+public class RemoteConstants {
+    public final static String URL_PROFILE = "https://www.facebook.com/profile.php?id=%s";
+}
---
+++ src/botvn/libraries/LoginInfo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package botvn.libraries;
+
+/**
+ *
+ * @author vanvo
+ */
+public class LoginInfo {
+    public String userId;
+    public String fbdtsg;
+}
